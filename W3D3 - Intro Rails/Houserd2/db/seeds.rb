faa353c328f2ef68449ffb59168e7c129db61a91
Angel Velasco <avelasco920@gmail.com>
2017-10-18 22:31:58 -0700

    created n1 buster project

--- W3D3 - Intro Rails/Houserd2/db/seeds.rb
+++ W3D3 - Intro Rails/Houserd2/db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-house1 = House.create(address: '219 29th Ave.')
-house2 = House.create(address: '372 Anza St.')
-house3 = House.create(address: '2 Harrison St.')
-
-angel = Person.create(name: 'Angel V.', house_id: 2)
-andrea = Person.create(name: 'Andrea C.', house_id: 2)
-tho = Person.create(name: 'Tho N.', house_id: 1)
-micahel = Person.create(name: 'Michael S', house_id: 3)
--- W3D3 - Intro Rails/Houses/test/models/house.rb
+++
@@ -1,8 +0,0 @@
-class House < ActiveRecord::Base
-  validates :address, present: true, uniqueness: true
-
-  has_many :people.
-    primary_key: :id,
-    foreign_key: :house_id,
-    class_name: 'Person'
-end
