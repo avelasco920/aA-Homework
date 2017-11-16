582024e63660cd8c9b7b7746f65cb547fcc2f21c
Angel Velasco <avelasco920@gmail.com>
2017-11-15 21:02:06 -0800

    added frontend routes

--- W7D4 - Rainbow Routes/skeleton/components/rainbow.jsx
+++ W7D4 - Rainbow Routes/skeleton/components/rainbow.jsx
@@ -15,10 +15,15 @@ class Rainbow extends React.Component {
     return (
       <div>
         <h1>Rainbow Router!</h1>
-        {/* Your links should go here */}
-
+          <NavLink exact to='/red' >Red only</NavLink>
+          <NavLink exact to='/green' >Green</NavLink>
+          <NavLink exact to='/blue' >Blue only</NavLink>
+          <NavLink exact to='/violet' >Violet</NavLink>
         <div id="rainbow">
-          {/* Your routes should go here */}
+          <Route path="/red" component={Red} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/violet" component={Violet} />
         </div>
       </div>
     );
