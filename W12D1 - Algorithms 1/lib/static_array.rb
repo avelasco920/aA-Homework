b5a5b54e64281e3cb9cb5ef808cca68e6dd715d2
Angel Velasco <avelasco920@gmail.com>
2017-12-31 20:34:40 -0800

    edits to dynamic arrays

--- W12D1 - Algorithms 1/lib/static_array.rb
+++ W12D1 - Algorithms 1/lib/static_array.rb
@@ -1,10 +1,12 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
   def initialize(length)
+    Array.new(length)
   end
 
   # O(1)
   def [](index)
+    
   end
 
   # O(1)
