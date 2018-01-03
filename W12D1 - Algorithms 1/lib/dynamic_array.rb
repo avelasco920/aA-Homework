c5be677a7b7a68ff48fab5748fc0af8013b07f78
Angel Velasco <avelasco920@gmail.com>
2018-01-02 16:37:24 -0800

    complete dynamic array

--- W12D1 - Algorithms 1/lib/dynamic_array.rb
+++ W12D1 - Algorithms 1/lib/dynamic_array.rb
@@ -1,34 +1,64 @@
 require_relative "static_array"
+require "byebug"
 
 class DynamicArray
   attr_reader :length
 
   def initialize
+    @capacity = 8
+    @length = 0
+    @store = StaticArray.new(@length)
   end
 
   # O(1)
   def [](index)
+    raise "index out of bounds" if index + 1 > @length
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" unless @store[0]
+    @store[@length - 1] = nil
+    @length -= 1
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @length == @capacity
+    @store[@length] = val
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" unless @store[0]
+    new_store = StaticArray.new(@length - 1)
+    last = @store[0]
+    (1..@length - 1).each do |idx|
+      new_store[idx - 1] = @store[idx]
+    end
+    @length -= 1
+    @store = new_store
+    last
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if @length == @capacity
+    new_store = StaticArray.new(@length + 1)
+    @length += 1
+    new_store[0] = val
+    (0..@length).each do |i|
+      new_store[i + 1] = @store[i]
+    end
+    @store = new_store
   end
 
   protected
@@ -40,5 +70,11 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    new_store = StaticArray.new(@capacity * 2)
+    (0..@capacity).each do |idx|
+      new_store[idx] = @store[idx]
+    end
+    @capacity *= 2
+    @store = new_store
   end
 end
--- W12D1 - Algorithms 1/lib/static_array.rb
+++ W12D1 - Algorithms 1/lib/static_array.rb
@@ -1,17 +1,17 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
   def initialize(length)
-    Array.new(length)
+    @store = Array.new(length)
   end
 
   # O(1)
   def [](index)
-    self.at(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
-    self[index] = value
+    @store[index] = value
   end
 
   protected
