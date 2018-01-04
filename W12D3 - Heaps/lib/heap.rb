94878ad584fafa4e7c08c1aaefeb18ccbf89b87f
Angel Velasco <avelasco920@gmail.com>
2018-01-04 01:17:34 -0800

    complete heap assignment

--- W12D3 - Heaps/lib/heap.rb
+++ W12D3 - Heaps/lib/heap.rb
@@ -11,6 +11,15 @@ class BinaryMinHeap
   end
 
   def extract
+    raise "no element to extract" if count == 0
+    val = store[0]
+    if count > 1
+      store[0] = store.pop
+      self.class.heapify_down(store, 0, &prc)
+    else
+      store.pop
+    end
+    val
   end
 
   def peek
@@ -18,15 +27,18 @@ class BinaryMinHeap
   end
 
   def push(val)
+    @store.push(val)
+    BinaryMinHeap.heapify_up(@store, count - 1, count, &@prc)
+    val
   end
 
   public
   def self.child_indices(len, parent_index)
     indicies = []
-    idx_1 = (parent_index * 2) + 1
-    idx_2 = (parent_index * 2) + 2
-    indicies << idx_1 if idx_1 < len
-    indicies << idx_2 if idx_2 < len
+    [1, 2].each do |n|
+      idx = (parent_index * 2) + n
+      indicies << idx if idx < len
+    end
     indicies
   end
 
@@ -37,9 +49,30 @@ class BinaryMinHeap
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
     prc ||= Proc.new { |a, b| a <=> b }
+    children = self.child_indices(len, parent_idx)
 
+    if children.all? { |child_idx| prc.call(array[parent_idx], array[child_idx]) <= 0 }
+      return array
+    elsif children.empty?
+      return array
+    elsif children.length == 1
+      smallest_child_idx = children[0]
+    else
+      smallest_child_idx = prc.call(array[children[0]], array[children[1]]) <= 0 ? children[0] : children[1]
+    end
+
+    array[parent_idx], array[smallest_child_idx] = array[smallest_child_idx], array[parent_idx]
+    self.heapify_down(array, smallest_child_idx, len, &prc)
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    return array if child_idx == 0
+    prc ||= Proc.new { |a, b| a <=> b }
+    parent_idx = self.parent_index(child_idx)
+    if prc.call(array[parent_idx], array[child_idx]) == 1
+      array[parent_idx], array[child_idx] = array[child_idx], array[parent_idx]
+      self.heapify_up(array, parent_idx, len, &prc)
+    end
+    array
   end
 end
--- W12D3 - Heaps/lib/k_largest_elements.rb
+++ W12D3 - Heaps/lib/k_largest_elements.rb
@@ -1,5 +1,13 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-
+  res = BinaryMinHeap.new
+  k.times do
+    res.push(array.pop)
+  end
+  until array.empty?
+    res.push(array.pop)
+    res.extract
+  end
+  res.store
 end
