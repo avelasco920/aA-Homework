5155551db69fe36b633dcaac397c3f331a0df5da
Angel Velasco <avelasco920@gmail.com>
2018-01-09 00:12:02 -0800

    complete graph data structure

--- W13D2 - Graphs and Topological Sort/lib/graph.rb
+++ W13D2 - Graphs and Topological Sort/lib/graph.rb
@@ -1,15 +1,30 @@
 class Vertex
+  attr_accessor :value, :in_edges, :out_edges
   def initialize(value)
-
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
+  attr_reader :from_vertex, :to_vertex, :cost
   def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    attach_to_verticies(from_vertex, to_vertex)
+  end
 
+  def attach_to_verticies(from_vertex, to_vertex)
+    from_vertex.out_edges.push(self)
+    to_vertex.in_edges.push(self)
   end
 
   def destroy!
-
+    @from_vertex.out_edges.delete(self)
+    @to_vertex.in_edges.delete(self)
+    @to_vertex = nil
+    @from_vertex = nil
   end
 end
--- W13D2 - Graphs and Topological Sort/lib/topological_sort.rb
+++ W13D2 - Graphs and Topological Sort/lib/topological_sort.rb
@@ -3,5 +3,21 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
-
+  sorted = []
+  queue = []
+  vertices.each do |vertex|
+    queue.push(vertex) if vertex.in_edges.empty?
+  end
+  until queue.empty?
+    current = queue.shift
+    sorted.push(current)
+    current.out_edges.each do |edge|
+      if edge.to_vertex.in_edges.length == 1
+        queue.push(edge.to_vertex)
+      end
+      edge.destroy!
+    end
+  end
+  # p sorted.map { |vertex| vertex.value }
+  p sorted
 end
