28c951c948bc40ac19802ef57087995cdefbc5e9
Angel Velasco <avelasco920@gmail.com>
2018-01-10 11:04:32 -0800

    dynamic programming progress

--- W13D3 - Dynamic Programming/lib/dynamic_programming.rb
+++ W13D3 - Dynamic Programming/lib/dynamic_programming.rb
@@ -1,27 +1,65 @@
 class DynamicProgramming
 
   def initialize
+    @blair_cache = {
+      0 => 0,
+      1 => 1,
+      2 => 2
+    }
 
+    @frog_cache_top_down = [
+      [],
+      [[1]],
+      [[1, 1], [2]],
+      [[1, 1, 1], [1, 2], [2, 1], [3]],
+    ]
   end
 
   def blair_nums(n)
+    return @blair_cache[n] if @blair_cache[n]
 
+    prev_blair = @blair_cache[n - 1] || blair_nums(n - 1)
+    prev_prev_blair = @blair_cache[n - 2] || blair_nums(n - 2)
+
+    result = prev_blair + prev_prev_blair + n * 2 - 3
+
+    @blair_cache[n] = result
+    result
   end
 
   def frog_hops_bottom_up(n)
-
+    frog_cache_builder(n)
+    @frog_cache[n]
   end
 
   def frog_cache_builder(n)
+    @frog_cache = [
+      [],
+      [[1]],
+      [[1, 1], [2]],
+      [[1, 1, 1], [1, 2], [2, 1], [3]],
+    ]
 
+    (4..n).each do |num|
+      @frog_cache[num] = @frog_cache[0...num].reduce(&:+)
+        .reject { |sub_arr| num - sub_arr.reduce(&:+) > 3 }
+        .map { |sub_arr| sub_arr + [num - sub_arr.reduce(&:+)] }
+    end
+
+    @frog_cache
   end
 
   def frog_hops_top_down(n)
-
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
-
+    return @frog_cache_top_down[n] if @frog_cache_top_down[n]
+    @frog_cache_top_down[n] = (0...n).map { |num| frog_hops_top_down(num) }
+      .reduce(&:+)
+      .reject { |sub_arr| n - sub_arr.reduce(&:+) > 3 }
+      .map { |sub_arr| sub_arr + [n - sub_arr.reduce(&:+)] }
+    @frog_cache_top_down[n]
   end
 
   def super_frog_hops(n, k)
